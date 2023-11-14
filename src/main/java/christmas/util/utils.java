f856f7906609463fe9733f1ce142069227561b90
geunsu <geunsu23@naver.com>
2023-11-15 08:38:37 +0900

    feat: 편의 기능 추가

---
+++ src/main/java/christmas/util/utils.java
@@ -0,0 +1,13 @@
+package christmas.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class utils {
+    private utils() {}
+
+    public static String addComma(int number) {
+        return String.format("%,d",number);
+    }
+
+}
