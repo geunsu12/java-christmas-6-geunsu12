254c7d3cc862078cb4fa7bc2d06640c715ca01af
geunsu <geunsu23@naver.com>
2023-11-15 17:07:31 +0900

    fix: 시작 출력문 엔터 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,7 +8,7 @@ public class OutputView {
     private OutputView() {};
 
     public static void printWelcomeMessage() {
-        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.%n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
     }
 
     public static void printGetOrderMessage() {
