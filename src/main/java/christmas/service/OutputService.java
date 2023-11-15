a480ff7d27aa052f426e00de72b59d4ccb5f63d6
geunsu <geunsu23@naver.com>
2023-11-15 09:57:12 +0900

    refactor: 함수 15줄넘기지 않기

--- src/main/java/christmas/service/OutputService.java
+++ src/main/java/christmas/service/OutputService.java
@@ -14,39 +14,35 @@ public class OutputService {
     public static String generateOutputMessage(Bill bill, Discount discount, Date date, Dishes dishes, Badge badge) {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n", date.getDate()));
-        sb.append("\n<주문 메뉴>\n");
         appendDishes(sb, dishes);
-        sb.append("\n<할인 전 총주문 금액>\n");
         appendTotalMoney(sb, bill);
-        sb.append("\n<증정 메뉴>\n");
         appendGift(sb, discount);
-        sb.append("\n<혜택 내역>\n");
         appendDiscountDetail(sb, discount);
-        sb.append("\n<총혜택 금액>\n");
         appendTotalDiscount(sb, discount);
-        sb.append("\n<할인 후 예상 결제 금액>\n");
         appendTotalPrice(sb, bill, discount);
-        sb.append("\n<12월 이벤트 배지>\n");
         appendBadge(sb, badge);
         return sb.toString();
     }
 
-
-    private static void appendTotalMoney(StringBuilder sb, Bill bill) {
-        sb.append(String.format("%,d원\n", bill.getTotalMoney()));
-    }
-
     private static void appendDishes(StringBuilder sb, Dishes dishes) {
+        sb.append("\n<주문 메뉴>\n");
         for (Map.Entry<String, Integer> eachDish : dishes.getDishes().entrySet()) {
             sb.append(String.format("%s %d개\n", eachDish.getKey(), eachDish.getValue()));
         }
     }
 
+    private static void appendTotalMoney(StringBuilder sb, Bill bill) {
+        sb.append("\n<할인 전 총주문 금액>\n");
+        sb.append(String.format("%,d원\n", bill.getTotalMoney()));
+    }
+
     private static void appendGift(StringBuilder sb, Discount discount) {
+        sb.append("\n<증정 메뉴>\n");
         sb.append(discount.isGivenGift());
     }
 
     private static void appendDiscountDetail(StringBuilder sb, Discount discount) {
+        sb.append("\n<혜택 내역>\n");
         boolean isNotDiscount = true;
         for(int i=0;i<5;i++) {
             int tmp = discount.getEachDiscount(i)*-1;
@@ -61,14 +57,17 @@ public class OutputService {
     }
 
     private static void appendTotalDiscount(StringBuilder sb, Discount discount) {
+        sb.append("\n<총혜택 금액>\n");
         sb.append(String.format("%,d원\n",discount.getTotalDiscountNGift()*-1));
     }
 
     private static void appendTotalPrice(StringBuilder sb, Bill bill, Discount discount) {
+        sb.append("\n<할인 후 예상 결제 금액>\n");
         sb.append( String.format("%,d원\n",bill.getTotalMoney() - discount.getTotalDiscount()));
     }
 
     private static void appendBadge(StringBuilder sb, Badge badge) {
+        sb.append("\n<12월 이벤트 배지>\n");
         sb.append(badge.getBadge());
     }
 }
